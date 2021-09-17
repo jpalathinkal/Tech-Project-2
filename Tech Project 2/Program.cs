4c866bc828f4dddb926f2ab655298c8b634ef2a2
Joshua Palathinkal <jpalathinkal@usf.edu>
2021-09-16 22:03:32 -0400

    version 2 of tech project 2

--- Tech Project 2/Program.cs
+++ Tech Project 2/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Author: Joshua Palathinkal
+// Date: 9/16/21
+// Description: Using C# conditional statements to output a letter grade after the user inputs an integer grade
+using System;
 
 namespace Tech_Project_2
 {
@@ -6,7 +9,46 @@ namespace Tech_Project_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Ask the user for input
+            Console.WriteLine("What grade do you expect to get in ISM 4300?");
+
+            try
+            {
+                string input = Console.ReadLine();
+
+                int grades = int.Parse(input);
+
+                if (grades < 60)
+                {
+                    Console.WriteLine("Your final letter grade would be an F.");
+                    Console.ReadKey(true);
+                }
+                else if ((grades > 59) && (grades < 70))
+                {
+                    Console.WriteLine("Your final letter grade would be a D.");
+                    Console.ReadKey(true);
+                }
+                else if ((grades > 69) && (grades < 80))
+                {
+                    Console.WriteLine("Your final letter grade would be a C.");
+                    Console.ReadKey(true);
+                }
+                else if ((grades > 79) && (grades < 90))
+                {
+                    Console.WriteLine("Your final letter grade would be a B.");
+                    Console.ReadKey(true);
+                }
+                else if ((grades > 89) && (grades < 101))
+                {
+                    Console.WriteLine("Your final letter grade would be an A.");
+                    Console.ReadKey(true);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer grade value between 0 and 100.");
+                Console.ReadKey(true);
+            }
         }
     }
 }
